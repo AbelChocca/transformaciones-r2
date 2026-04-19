30976c5f6aa940998372850e705bb8364c456bc0
AbelChocca <abelchocca1010@gmail.com>
2026-04-19 14:07:59 -0500

    feat: update Scale class to support independent scaling factors for x and y axes

--- transformaciones-r2/transformations/scale.py
+++ transformaciones-r2/transformations/scale.py
@@ -1,15 +1,17 @@
 from transformations.base import Transformation
 import numpy as np
 from numpy.typing import NDArray
+from typing import Optional
 
 class Scale(Transformation):
-    def __init__(self, factor: float):
-        self.factor = factor
+    def __init__(self, kx: float, ky: Optional[float] = None):
+        self.kx = kx
+        self.ky = ky if ky is not None else kx  # uniforme si no se pasa ky
         
     def apply(self, points: NDArray[np.float64]) -> NDArray[np.float64]:
         scale_matrix = np.array([
-            [self.factor, 0],
-            [0, self.factor]
+            [self.kx, 0],
+            [0, self.ky]
         ])
 
         return points @ scale_matrix.T
